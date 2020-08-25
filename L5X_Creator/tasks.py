f3b1467fcd57f0d13e3750b6e46315545f67d06a
coltman151 <65498634+coltman151@users.noreply.github.com>
2020-08-25 13:00:31 -0400

    Added TODO to tag program

---
+++ L5X_Creator/tasks.py
@@ -0,0 +1,29 @@
+"""Creates tasks in program"""
+
+#TODO FIX THIS AFTER BUILDING PROGRAM GENERATOR AS IT'LL
+#NEED TO PULL FROM THE LIST OF CREATED PROGRAMS AND WON'T BE A SIMPLE
+#CREATE AND PUSH TO XML. Code below is placeholder and needs fixed/removed
+#needs to call the programs class after creating a task to ensure programs
+#are written for specific task
+
+# import xml.etree.ElementTree as ET
+# from indent import indent
+
+# class BuildTasks():
+#     """Builds the project tasks from input function"""
+#     def __init__(self,
+#                  project_path,
+#                  task_name,
+#                  task_type,
+#                  priority,
+#                  radix,
+#                  constant,
+#                  external_access,
+#                 ):
+#         self.project_path = project_path
+#         self.tag_name = tag_name
+#         self.tag_type = tag_type
+#         self.data_type = data_type
+#         self.radix = radix
+#         self.constant = constant
+#         self.external_access = external_access
